06fb6b6cd7ddaf8537922e55e5f53aaa68bce539
Marc Brinkmann <git@marcbrinkmann.de>
2010-07-11 13:00:41 +0200

    Fixed broken code resulting from error in create-gobject.py.

--- clutterdesktopwidget.h
+++ clutterdesktopwidget.h
@@ -6,11 +6,11 @@
 #include <gtk/gtk.h>
 
 #define CLUTTER_TYPE_DESKTOP_WIDGET (clutter_desktop_widget_get_type())
-#define CLUTTER_DESKTOP_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), CLUTTER_TYPE_DESKTOP_WIDGET, DesktopWidget))
+#define CLUTTER_DESKTOP_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), CLUTTER_TYPE_DESKTOP_WIDGET, ClutterDesktopWidget))
 #define CLUTTER_IS_DESKTOP_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), CLUTTER_TYPE_DESKTOP_WIDGET))
-#define CLUTTER_DESKTOP_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), CLUTTER_TYPE_DESKTOP_WIDGET, DesktopWidgetClass))
+#define CLUTTER_DESKTOP_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), CLUTTER_TYPE_DESKTOP_WIDGET, ClutterDesktopWidgetClass))
 #define CLUTTER_IS_DESKTOP_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), CLUTTER_TYPE_DESKTOP_WIDGET))
-#define CLUTTER_DESKTOP_WIDGET_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), CLUTTER_TYPE_DESKTOP_WIDGET, DesktopWidgetClass))
+#define CLUTTER_DESKTOP_WIDGET_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), CLUTTER_TYPE_DESKTOP_WIDGET, ClutterDesktopWidgetClass))
 
 typedef struct _ClutterDesktopWidget ClutterDesktopWidget;
 typedef struct _ClutterDesktopWidgetClass ClutterDesktopWidgetClass;
